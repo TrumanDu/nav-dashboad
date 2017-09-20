8101282587d408d633ce33e82c53bde70a2057a7
truman <truman.p.du@newegg.com>
2017-09-20 18:48:37 +0800

    init

--- src/controller/index.js
+++ src/controller/index.js
@@ -4,10 +4,40 @@ module.exports = class extends Base {
     async indexAction() {
         let model = this.model('index');
         let data = await model.select();
-        //data returns [{name: 'thinkjs', email: 'admin@thinkjs.org'}, ...]
-        //return this.json(data);
         this.assign('indexs', data); //给模板赋值
         return this.display();
+    }
+
+    async searchAction() {
+        let searchName = this.post('searchName');
+        let model = this.model('index');
+        if (!think.isTrueEmpty(searchName)) {
+            let data = await model.where({name: ['like', '%' + searchName + '%']}).select();
+            if (!think.isTrueEmpty(data)) {
+                let searchData = null;
+                if (data.length == 0) {
+                    searchData = await model.where({id: data.id}).select();
+                } else {
+                    let ids = "";
+                    data.forEach(function (info) {
+                        if (think.isTrueEmpty(ids)) {
+                            ids = info.pid + "," + info.id;
+                        } else {
+                            ids = ids + "," + info.pid + "," + info.id;
+                        }
+                    });
+                    console.log(ids);
+                    searchData = await model.where({id: ['IN', ids]}).select();
+                }
+                this.assign('indexs', searchData); //给模板赋值
+            }
+        } else {
+            let data = await model.select();
+            this.assign('indexs', data); //给模板赋值
+        }
+        this.assign('searchName', searchName); //给模板赋值
+        return this.display("index_index");
+
     }
 
     /**
@@ -31,16 +61,42 @@ module.exports = class extends Base {
     }
 
     /**
+     * add
+     * @returns {Promise.<*>}
+     */
+    async subclassAddAction() {
+        let name = this.post('sname');
+        let url = this.post('url');
+        let pid = this.post('pid');
+        let level = this.post('level');
+
+        let model = this.model('index');
+        let id = await model.add({
+            name: name,
+            url: url,
+            level: level,
+            pid: pid
+        });
+        this.body = "add success！ID:" + id;
+        if (id) {
+            return this.success(id, "add success!");
+        } else {
+            return this.fail(1000, "add fail!");
+        }
+        this.body = "add success！ID:" + id;
+    }
+
+    /**
      * delete
      * @returns {Promise.<void>}
      */
-    async deleteAction(){
+    async deleteAction() {
         let id = this.get('id');
         let model = this.model('index');
-        let result = await model.where({id:['=',id]}).delete();
+        let result = await model.where({id: ['=', id]}).delete();
     }
 
-    async listAction(){
+    async listAction() {
         let model = this.model('index');
         let data = await model.select();
         //data returns [{name: 'thinkjs', email: 'admin@thinkjs.org'}, ...]
